9c8883a6ffbafd2e483e0f145f184e181b3af010
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-21 02:03:40 +0200

    added python solution

--- 01-sockets-basic-server/python/client.py
+++ 01-sockets-basic-server/python/client.py
@@ -2,15 +2,23 @@ import socket
 
 from shared import *
 
-# Safe handling of resources that require releasing on exceptions.
-with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as client_socket:
 
-    client_socket.connect ((SERVER_ADDR, SERVER_PORT))
-    client_socket.send ('Hello !'.encode ('utf-8'))
-    print ('Received message:', client_socket.recv (SOCKET_BUFFER_SIZE).decode ('utf-8'))
+# Safe handling of resources that require releasing on exceptions.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+
+    client_socket.connect((SERVER_ADDR, SERVER_PORT))
+    
+    data = client_socket.recv(SOCKET_BUFFER_SIZE)
+    if data:
+        i_size = data[0]
+        tm_size = data[1]
+
+        ctm : time.struct_time = parse_tm(data, i_size, tm_size, 2)
+
+        print(time.asctime(ctm_to_tm(ctm)))
 
     # Shutdown precedes close to make sure protocol level shutdown is executed completely.
     # Close without shutdown may use RST instead of FIN to terminate connection, dropping data that is in flight.
     #
     # It is also possible to use shutdown to close input and output streams independently.
-    client_socket.shutdown (socket.SHUT_RDWR)
+    client_socket.shutdown(socket.SHUT_RDWR)
--- 01-sockets-basic-server/python/server.py
+++ 01-sockets-basic-server/python/server.py
@@ -3,30 +3,26 @@ import socket
 from shared import *
 
 # Safe handling of resources that require releasing on exceptions.
-with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-    server_socket.bind (('', SERVER_PORT))
-    server_socket.listen (SERVER_SOCKET_BACKLOG)
+    server_socket.bind(('', SERVER_PORT))
+    server_socket.listen(SERVER_SOCKET_BACKLOG)
 
-    print ('Waiting for incoming connection.')
+    print('Waiting for incoming connection.')
 
     while True:
 
         # Safe handling of resources that require releasing on exceptions.
-        with server_socket.accept () [0] as client_socket:
+        with server_socket.accept()[0] as client_socket:
 
-            print ('Accepted an incoming connection.')
+            print('Accepted an incoming connection.')
 
-            while True:
-                data = client_socket.recv (SOCKET_BUFFER_SIZE)
-                if len (data) == 0:
-                    break
-                client_socket.send (data)
+            client_socket.send(pack_tm(tm_to_ctm(make_tm())))
 
-            print ('Client disconnected.')
+            print('Client disconnected.')
 
             # Shutdown precedes close to make sure protocol level shutdown is executed completely.
             # Close without shutdown may use RST instead of FIN to terminate connection, dropping data that is in flight.
             #
             # It is also possible to use shutdown to close input and output streams independently.
-            client_socket.shutdown (socket.SHUT_RDWR)
+            client_socket.shutdown(socket.SHUT_RDWR)
