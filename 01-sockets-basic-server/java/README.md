b5a84d28e56246981b959229519b3dd9ef0cc07f
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-21 02:08:34 +0200

    removed java

--- 01-sockets-basic-server/java/README.md
+++
@@ -1,9 +0,0 @@
-# Socket Based Server
-
-## Running the example
-
-```
-> mvn compile
-> mvn exec:java@server &
-> mvn exec:java@client
-```
--- 01-sockets-basic-server/java/src/main/java/Shared.java
+++
@@ -1,4 +0,0 @@
-class Shared {
-    public static final String SERVER_ADDR = "127.0.0.1";
-    public static final int SERVER_PORT = 8888;
-}
