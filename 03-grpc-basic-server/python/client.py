352411a39a972b49f8006fa92722bd027c33a9a8
jiriklepl <jiriklepl@seznam.cz>
2019-11-21 23:29:52 +0100

    added python solution

--- 03-grpc-basic-server/python/client.py
+++ 03-grpc-basic-server/python/client.py
@@ -1,25 +1,45 @@
 import grpc
+import sys
+
+sys.path.append('.')
 
 from shared import *
 
-from example_pb2 import *
-from example_pb2_grpc import *
+from proto.time_pb2 import *
+from proto.time_pb2_grpc import *
 
 
 # Create the channel used to connect to the server.
 with grpc.insecure_channel (SERVER_ADDR) as channel:
 
     # Create a stub object that provides the service interface.
-    stub = AnExampleServiceStub (channel)
+    stub = TimeServiceStub (channel)
 
-    message = AnExampleMessage ()
-    message.some_integer = 0xDEAD
-    message.some_string = 'Hello gRPC !'
-    print ('Message:')
-    print (message)
+    request = TimeRequest()
+    for arg in sys.argv[1:]:
+        if arg == 'seconds':
+            request.seconds = True
+        elif arg == 'minutes':
+            request.minutes = True
+        elif arg == 'hours':
+            request.hours = True
+        elif arg == 'mday':
+            request.mday = True
+        elif arg == 'month':
+            request.month = True
+        elif arg == 'year':
+            request.year = True
+        elif arg == 'wday':
+            request.wday = True
+        elif arg == 'yday':
+            request.yday = True
+        elif arg == 'isdst':
+            request.isdst = True
+    print('Message:')
+    print(request)
 
     # Call the service through the stub object.
-    response = stub.CloneMessage (message)
+    response = stub.GetTime (request)
 
-    print ('Response:')
-    print (response)
+    print('Response:')
+    print(response)
