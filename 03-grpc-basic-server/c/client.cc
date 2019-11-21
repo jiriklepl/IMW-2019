45ba29b90450aafe657a48db5e67280418774238
jiriklepl <jiriklepl@seznam.cz>
2019-11-21 23:30:32 +0100

    added c++ solution

--- 03-grpc-basic-server/c/client.cc
+++ 03-grpc-basic-server/c/client.cc
@@ -1,39 +1,63 @@
 #include <string>
+#include <string_view>
 #include <iostream>
+#include <utility>
 
 #include <grpc++/grpc++.h>
-// To make the API calls easily visible, the example does not use the grpc namespace.
-// A standard application would likely be "using namespace grpc" here.
 
 #include "shared.h"
+#include "time.grpc.pb.h"
 
-#include "example.grpc.pb.h"
-using namespace example;
+using namespace grpc;
+using namespace time_message;
 
 
-int main ()
-{
+int main(int argc, const char* argv[]) {
     // Create the channel used to connect to the server.
-    std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel (SERVER_ADDR, grpc::InsecureChannelCredentials ());
+    std::shared_ptr<Channel> channel = CreateChannel(
+        SERVER_ADDR,
+        InsecureChannelCredentials());
 
     // Create a stub object that provides the service interface.
-    std::shared_ptr<AnExampleService::Stub> stub = AnExampleService::NewStub (channel);
+    std::shared_ptr<TimeService::Stub> stub = TimeService::NewStub(channel);
 
-    AnExampleMessage message;
-    message.set_some_integer (0xDEAD);
-    message.set_some_string ("Hello gRPC !");
+    TimeRequest request;
+    for (const char** argp = argv + 1; *argp != nullptr; ++argp) {
+        std::string_view arg = *argp;
+
+        if (arg == "seconds") {
+            request.set_seconds(true);
+        } else if (arg == "minutes") {
+            request.set_minutes(true);
+        } else if (arg == "hours") {
+            request.set_hours(true);
+        } else if (arg == "mday") {
+            request.set_mday(true);
+        } else if (arg == "month") {
+            request.set_month(true);
+        } else if (arg == "year") {
+            request.set_year(true);
+        } else if (arg == "wday") {
+            request.set_wday(true);
+        } else if (arg == "yday") {
+            request.set_yday(true);
+        } else if (arg == "isdst") {
+            request.set_isdst(true);
+        }
+    }
+
     std::cout << "Message:" << std::endl;
-    std::cout << message.DebugString () << std::endl;
+    std::cout << request.DebugString() << std::endl;
 
-    grpc::ClientContext context;
-    MoreExampleMessages response;
+    ClientContext context;
+    TimeMessage response;
 
     // Call the service through the stub object.
-    grpc::Status status = stub->CloneMessage (&context, message, &response);
+    Status status = stub->GetTime(&context, request, &response);
 
-    if (status.ok ()) {
+    if (status.ok()) {
         std::cout << "Response:" << std::endl;
-        std::cout << response.DebugString () << std::endl;
+        std::cout << response.DebugString() << std::endl;
     }
 
     return (0);
--- 03-grpc-basic-server/c/server.cc
+++ 03-grpc-basic-server/c/server.cc
@@ -1,28 +1,69 @@
 #include <grpc++/grpc++.h>
-// To make the API calls easily visible, the example does not use the grpc namespace.
-// A standard application would likely be "using namespace grpc" here.
+#include <ctime>
 
 #include "shared.h"
 
-#include "example.grpc.pb.h"
-using namespace example;
+#include "time.grpc.pb.h"
+using namespace grpc;
+using namespace time_message;
 
 
 // Service implementation.
 //
 // The implementation inherits from a generated base class.
 
-class MyService : public AnExampleService::Service {
-    grpc::Status CloneMessage (grpc::ServerContext *context, const AnExampleMessage *request, MoreExampleMessages *response) override {
-        response->add_messages ()->CopyFrom (*request);
-        response->add_messages ()->CopyFrom (*request);
-        return (grpc::Status::OK);
+class MyService : public TimeService::Service {
+    Status GetTime(
+        ServerContext *context,
+        const TimeRequest *request,
+        TimeMessage *response
+    ) override {
+        time_t rawtime;
+        time(&rawtime);
+        tm* now = localtime(&rawtime);
+
+        if (request->seconds()) {
+            response->set_seconds(now->tm_sec);
+        }
+
+        if (request->minutes()) {
+            response->set_minutes(now->tm_min);
+        }
+
+        if (request->hours()) {
+            response->set_hours(now->tm_hour);
+        }
+
+        if (request->mday()) {
+            response->set_mday(now->tm_mday);
+        }
+
+        if (request->month()) {
+            response->set_month(now->tm_mon);
+        }
+
+        if (request->year()) {
+            response->set_year(now->tm_year);
+        }
+
+        if (request->wday()) {
+            response->set_wday(now->tm_wday);
+        }
+
+        if (request->yday()) {
+            response->set_yday(now->tm_yday);
+        }
+
+        if (request->isdst()) {
+            response->set_isdst(now->tm_isdst);
+        }
+
+        return Status::OK;
     }
 };
 
 
-int main ()
-{
+int main() {
     // Create the server object.
     //
     // The server object represents the server runtime.
@@ -30,15 +71,15 @@ int main ()
     // and what port to listen on.
 
     MyService service;
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort (SERVER_ADDR, grpc::InsecureServerCredentials ());
-    builder.RegisterService (&service);
-    std::unique_ptr<grpc::Server> server (builder.BuildAndStart ());
+    ServerBuilder builder;
+    builder.AddListeningPort(SERVER_ADDR, InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
 
     // The server is never asked to terminate in this example,
     // it therefore waits here until interrupted from outside.
 
-    server->Wait ();
+    server->Wait();
 
-    return (0);
+    return 0; 
 }
