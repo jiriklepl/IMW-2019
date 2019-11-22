3ae2d2fe67e77e2c92a6fb7226b9d7525682a5c1
jiriklepl <jiriklepl@seznam.cz>
2019-11-22 12:18:00 +0100

    update

--- 04-jgroups-basic-peer/java/src/main/java/Peer.java
+++ 04-jgroups-basic-peer/java/src/main/java/Peer.java
@@ -4,13 +4,6 @@ import org.jgroups.JChannel;
 import org.jgroups.Message;
 
 
-import java.io.Serializable;
-
-public class UpdateEvent implements Serializable {
-    private static final long serialVersionUID = 0xBAADBAADBAADL;
-    public int key;
-    public String value;
-}
 
 public class Peer {
 
---
+++ 04-jgroups-basic-peer/java/src/main/java/UpdateEvent.java
@@ -0,0 +1,12 @@
+
+import java.io.Serializable;
+
+public class UpdateEvent implements Serializable {
+    public UpdateEvent(int new_key, String new_string) {
+        key = new_key;
+        value = new_string;
+    } 
+    private static final long serialVersionUID = 0xBAADBAADBAADL;
+    public int key;
+    public String value;
+}
