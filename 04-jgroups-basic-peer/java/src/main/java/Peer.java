974b1c2d0f30de1028a7bdaac3aea696820fde62
jiriklepl <jiriklepl@seznam.cz>
2019-11-22 12:07:46 +0100

    update

--- 04-jgroups-basic-peer/java/src/main/java/Peer.java
+++ 04-jgroups-basic-peer/java/src/main/java/Peer.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 
+
+import java.io.Serializable;
+
+public class UpdateEvent implements Serializable {
+    private static final long serialVersionUID = 0xBAADBAADBAADL;
+    public int key;
+    public String value;
+}
+
 public class Peer {
 
     public static void main (String [] arguments) {
         try {
             JChannel channel = new JChannel ();
-            channel.connect ("ExampleCluster");
+            channel.connect ("SharedHashMap");
             channel.setReceiver (new ReceiverEventHandler ());
 
             Scanner scanner = new Scanner (System.in);
             while (true) {
-                String line = scanner.nextLine ();
+                UpdateEvent line = new UpdateEvent{0, "message"};
                 Message message = new Message (null, line);
                 channel.send (message);
             }
