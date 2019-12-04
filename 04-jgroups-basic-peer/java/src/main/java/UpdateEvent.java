f62c6c5cbb33ece0cb878e8984d0fd9f33fbd170
jiriklepl <jiriklepl@seznam.cz>
2019-12-04 20:47:27 +0100

    style

--- 04-jgroups-basic-peer/java/src/main/java/UpdateEvent.java
+++ 04-jgroups-basic-peer/java/src/main/java/UpdateEvent.java
@@ -5,7 +5,8 @@ public class UpdateEvent implements Serializable {
     public UpdateEvent(int new_key, String new_string) {
         key = new_key;
         value = new_string;
-    } 
+    }
+
     private static final long serialVersionUID = 0xBAADBAADBAADL;
     public int key;
     public String value;
