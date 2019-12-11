84c24d182a0e94f905865a31022e6febb05090f8
jiriklepl <jiriklepl@seznam.cz>
2019-12-11 15:31:26 +0100

    progress on 05-secure

--- 05-grpc-secure-server/python/client.py
+++ 05-grpc-secure-server/python/client.py
@@ -2,9 +2,8 @@ import grpc
 
 from shared import *
 
-from example_pb2 import *
-from example_pb2_grpc import *
-
+from time_pb2 import *
+from time_pb2_grpc import *
 
 # Create the channel used to connect to the server.
 crt_data = open ('server.crt', 'rb').read ()
@@ -12,16 +11,33 @@ credentials = grpc.ssl_channel_credentials (root_certificates = crt_data)
 with grpc.secure_channel (SERVER_ADDR, credentials) as channel:
 
     # Create a stub object that provides the service interface.
-    stub = AnExampleServiceStub (channel)
+    stub = TimeServiceStub (channel)
 
-    message = AnExampleMessage ()
-    message.some_integer = 0xDEAD
-    message.some_string = 'Hello gRPC !'
-    print ('Message:')
-    print (message)
+    request = TimeRequest()
+    for arg in sys.argv[1:]:
+        if arg == 'seconds':
+            request.seconds = True
+        elif arg == 'minutes':
+            request.minutes = True
+        elif arg == 'hours':
+            request.hours = True
+        elif arg == 'mday':
+            request.mday = True
+        elif arg == 'month':
+            request.month = True
+        elif arg == 'year':
+            request.year = True
+        elif arg == 'wday':
+            request.wday = True
+        elif arg == 'yday':
+            request.yday = True
+        elif arg == 'isdst':
+            request.isdst = True
+    print('Message:')
+    print(request)
 
     # Call the service through the stub object.
-    response = stub.CloneMessage (message)
+    response = stub.GetTime (request)
 
-    print ('Response:')
-    print (response)
+    print('Response:')
+    print(response)
--- 05-grpc-secure-server/python/server.py
+++ 05-grpc-secure-server/python/server.py
@@ -5,19 +5,35 @@ from concurrent import futures
 
 from shared import *
 
-from example_pb2 import *
-from example_pb2_grpc import *
-
+from time_pb2 import *
+from time_pb2_grpc import *
 
 # Service implementation.
 #
 # The implementation inherits from a generated base class.
 
-class MyServicer (AnExampleServiceServicer):
-    def CloneMessage (self, request, context):
-        response = MoreExampleMessages ()
-        response.messages.add ().CopyFrom (request)
-        response.messages.add ().CopyFrom (request)
+class MyServicer (TimeServiceServicer):
+    def GetTime (self, request, context):
+        now = time.localtime(time.time())
+        response = TimeMessage ()
+        if request.seconds:
+            response.seconds = now.tm_sec
+        if request.minutes:
+            response.minutes = now.tm_min
+        if request.hours:
+            response.hours = now.tm_hour
+        if request.mday:
+            response.mday = now.tm_mday
+        if request.month:
+            response.month = now.tm_mon
+        if request.year:
+            response.year = now.tm_year
+        if request.wday:
+            response.wday = now.tm_wday
+        if request.yday:
+            response.yday = now.tm_yday
+        if request.isdst:
+            response.isdst = now.tm_isdst
         return response
 
 
